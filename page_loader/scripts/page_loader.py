38575bb2201d361bfc125bf80c6fe3b5123101b7
veetors <veetor.s@gmail.com>
2020-05-10 18:50:44 +0300

    add parser for cli-args & update package version

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -3,14 +3,24 @@
 
 """Package entry point."""
 
+import argparse
+
 import page_loader
 
 
 def main():
     """Run main function."""
-    output = page_loader.load('https://hexlet.io/courses')
+    parser = argparse.ArgumentParser(description='Download the internet page')
+    parser.add_argument('url')
+    parser.add_argument(
+        '-O',
+        '--output',
+        type=str,
+        help='path to output folder (default: current working directory)',
+    )
+    args = parser.parse_args()
 
-    print(output)
+    page_loader.load(args.url, args.output)
 
 
 if __name__ == '__main__':
